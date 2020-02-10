7c4ecfb22346683bcd475862463c50b5c7cff569
ask4shahzaib <shahzaibali008@hotmail.com>
2020-02-10 20:26:22 +0500

    DFS working properly

--- DFS.py
+++ DFS.py
@@ -23,8 +23,9 @@ class Position:
 
 
 def dfs(visited, queue, node):
-    cont = 1
+    res = 0
     if node.x == endrow and node.y == endcol:
+        res = 1
         print("Path found")
         temp = node
         while(temp.parent != None):
@@ -35,6 +36,7 @@ def dfs(visited, queue, node):
         print(" ")
         for i in range (rows):
             print(grid[i])
+        return res
     else:
             s = node
             visited.append(s)
@@ -43,36 +45,33 @@ def dfs(visited, queue, node):
                     print("The final state can't be reached there is a hurdle there")
                     cont = 0
                 else:
-                    if grid[s.x - 1][s.y] != '1':
+                    if grid[s.x - 1][s.y] != '1' and res == 0:
                         one = Position(s.x - 1, s.y, s.cost + 2, grid[s.x - 1][s.y], s)
                         if one not in visited:
                          queue.append(one)
-                        new = queue.pop()
-                        dfs(visited, queue, new)
+                         res = dfs(visited, queue,one)
             if s.y + 1 < cols :
                 if (s.x  == endrow and s.y + 1 == endcol) and grid[s.x][s.y + 1] == '1':
                     print("The final state can't be reached there is a hurdle there")
                     cont = 0
                 else:
-                    if grid[s.x][s.y + 1] != '1':
+                    if grid[s.x][s.y + 1] != '1' and res == 0:
                         two = Position(s.x, s.y + 1, s.cost + 2, grid[s.x][s.y + 1], s)
                         if two not in visited:
                          queue.append(two)
-                        new = queue.pop()
-                        dfs(visited, queue, new)
+                         res = dfs(visited, queue, two)
             if (s.x - 1 >= 0 and s.y + 1 < cols):
                 if (s.x - 1  == endrow and s.y + 1 == endcol) and grid[s.x - 1][s.y + 1] == '1':
                     print("The final state can't be reached there is a hurdle there")
                     cont = 0
                 else:
-                    if grid[s.x - 1][s.y + 1] != '1':
+                    if grid[s.x - 1][s.y + 1] != '1' and res == 0:
                         three = Position(s.x - 1, s.y + 1, s.cost + 3, grid[s.x - 1][s.y + 1], s)
                         if three not in visited:
                          queue.append(three)
-                        new = queue.pop()
-                        dfs(visited, queue, new)
+                         res = dfs(visited, queue, three)
             else:
-                print("No path found")
+                queue.pop()
 for each in file:
     if i == 0:
         splitted = each.split()
@@ -98,9 +97,8 @@ if startrow < endrow:
 else:
     if(grid[startrow][startcol] != '1'):
         node = Position(startrow,startcol,0,grid[startrow][startcol],None)
+        queue.append(node)
         dfs(visited,queue,node)
-        if not queue:
-                print("No path available I am sorry")
     else:
         print("Starting position is a hurdle I am sorry")
 
